27859903ebbd07c772fb4f154a94abfdbaf23e00
Jom Kuriakose <kuriakose.jom@gmail.com>
2023-02-10 21:19:42 +0530

    plotted table5, 6 and 7

--- plot_tables_v3.py
+++ plot_tables_v3.py
@@ -1,26 +1,40 @@
+"""
+Code to plot tables to bar plot
+JNMR paper tables
+
+Author: Jom Kuriakose
+"""
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 # filename = 'tables/Table5_Mohana.txt'
 # Raga_name = "Mohana"
+# parent_lab = "(*)"
 
 # filename = 'tables/Table5_Madhyamavati.txt'
 # Raga_name = "Madhyamavati"
+# parent_lab = "(*)"
 
-# filename = 'tables/Table7_Gambhiranata.txt'
-# Raga_name = "Gambhiranata"
+filename = 'tables/Table7_Gambhiranata.txt'
+Raga_name = "Gambhiranata"
+parent_lab = "(**)"
 
-filename = 'tables/Table7_Amritavarshini.txt'
-Raga_name = "Amritavarshini"
+# filename = 'tables/Table7_Amritavarshini.txt'
+# Raga_name = "Amritavarshini"
+# parent_lab = "(*)"
 
 # yaxis limits -- set ylim to None for default values
 # ylim = None
 ylim = [0,0.6]
+# ylim = [0,1]
 
 # set width of bar
 # barWidth = 0.15
-barWidth = 0.10
+# barWidth = 0.10
+barWidth = 0.08
 
 # set height and width of figure
 height = 6
@@ -29,6 +43,7 @@ width = 12
 
 sym_list = [".","//","o","\\","*","xx","..",".O","++"]
 color_list = ["gray","darkgray","dimgray","lightgray","slategrey","grey","darkgrey","dimgrey","lightgrey"]
+color_list_new = list(mcolors.TABLEAU_COLORS.values())
 
 def read_table(filename):
         with open(filename, 'r') as fid:
@@ -59,20 +74,23 @@ def plot_figure(barWidth, height, width, sym_list, color_list, janaka_list, Raga
         for i in range(0,len(janaka_list)):
                 raga_label = list(janaka_list.keys())[i]
                 print(raga_label)
-                barlist = plt.bar(br_list[i], janaka_list[raga_label], color = color_list[i], width = barWidth, edgecolor ='black', label = raga_label)
-                for j in range(0, len(barlist)):
-                        barlist[j].set_hatch(sym_list[sym_counter])
+                if parent_lab in raga_label:
+                        barlist = plt.bar(br_list[i], janaka_list[raga_label], color = 'black', width = barWidth, edgecolor ='gray', label = raga_label)
+                else:
+                        barlist = plt.bar(br_list[i], janaka_list[raga_label], color = color_list[i], width = barWidth, edgecolor ='black', label = raga_label)
+                        for j in range(0, len(barlist)):
+                                barlist[j].set_hatch(sym_list[sym_counter])
                 sym_counter += 1
         
-        plt.title(f'Distance of Raga {Raga_name} with Parent Ragas', fontweight ='bold', fontsize = 15)
+        plt.title(f'Distance of Raga {Raga_name} with Parent Ragas', fontweight ='bold', fontsize = 10)
         # Adding Xticks
-        plt.xlabel('Distance Metric', fontweight ='bold', fontsize = 15)
-        plt.ylabel('Distance Value', fontweight ='bold', fontsize = 15)
-        plt.xticks([r + barWidth*(len(janaka_list)-1)/2 for r in range(len(janaka_list[list(janaka_list.keys())[0]]))], ['D(e)', 'D(bh)', 'D(kld)', 'D(js)'], fontsize = 12)
+        plt.xlabel('Distance Metric', fontweight ='bold', fontsize = 10)
+        plt.ylabel('Distance Value', fontweight ='bold', fontsize = 10)
+        plt.xticks([r + barWidth*(len(janaka_list)-1)/2 for r in range(len(janaka_list[list(janaka_list.keys())[0]]))], [f"D(e)\nEuclidean\nDistance", f"D(bh)\nBhattacharya-Hellinger\nDistance", f"D(kld)\nKullback-Leiber\nDivergence", f"D(js)\nJensen-Shannon\nDistance"], fontsize = 8)
 
         if ylim:
                 plt.ylim(ylim[0], ylim[1])
-        plt.legend(fontsize=12)
+        plt.legend(fontsize=10)
         final_fig = plt.gcf()
         plt.show()
         plt.draw()
@@ -84,3 +102,4 @@ if __name__ == '__main__':
         janaka_list = read_table(filename)
         file_name = os.path.splitext(os.path.basename(filename))
         plot_figure(barWidth, height, width, sym_list, color_list, janaka_list, Raga_name, ylim, 'plots/'+file_name[0])
+        plot_figure(barWidth, height, width, sym_list, color_list_new, janaka_list, Raga_name, ylim, 'plots/'+file_name[0]+'_color')
